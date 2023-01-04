b55c0590c568a08cb0ba4bfa708950ddd49997ed
J0rgeM <jorgemvsilva2002@gmail.com>
2023-01-04 20:28:29 +0000

    unit tests

---
+++ client/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Index() {
+    return (
+        <div className="footer text-center fixed-bottom text-white bg-dark">
+            <div className="container-fluid">
+                <div className="row">
+                    <p className="text-center">© 2022 DAW</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Index;

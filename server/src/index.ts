f92fa7b47d23ba1bfdfb7a493f97f9bb8688aba4
loisramses <rluisramos666@gmail.com>
2023-01-05 13:56:01 +0000

    comentarios sobre o frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,9 +9,14 @@ const app : Express = express();
 // built-in middleware for json
 app.use(express.json());
 
-//serve static files
+//endpoint "/"
+//express.static() - serve para fornecer os recursos estaticos (html, images, CSS files, and JavaScript files, etc.)
+//path.join() - junta os dois argumentos e poe normal o resultado do caminho para irmos buscar um ficheiro estatico a nossa maquina
+//__dirname - e o caminho da pasta atual
 app.use("/", express.static(path.join (__dirname, "../../client/dist")));
 
+//esta funcao adiciona os headers necessarios a resposta
+//inNext() - passa a proxima funcao que esta em stack do middleware
 app.use(function(inRequest: Request, inResponse: Response, inNext : NextFunction ) {
     inResponse.header("Access-Control-Allow-Origin", "*");
     inResponse.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
@@ -21,6 +26,8 @@ app.use(function(inRequest: Request, inResponse: Response, inNext : NextFunction
 
 const microposts: Posts.Microposts = new Posts.Microposts();
 
+//Registro do path e do method para o endpoint que é utilizado para obter a lista de posts.
+//app.get() - Routes HTTP GET requests to the specified path with the specified callback functions
 app.get("/", async (inRequest: Request ,inResponse: Response ) => {
     try {
         const post: IPost[] = await microposts.getPosts();
@@ -30,6 +37,8 @@ app.get("/", async (inRequest: Request ,inResponse: Response ) => {
     }
 });
 
+//Registro do path e método para o endpoint utilizado para adicionar posts, sendo que o path é /addPost
+//app.post() function routes the HTTP POST requests to the specified path with the specified callback functions
 app.post("/addPost", async (inRequest: Request ,inResponse: Response ) => {
     try {
         // foi criada esta variável para passar o que se recebe para argumento para IPost
@@ -45,6 +54,8 @@ app.post("/addPost", async (inRequest: Request ,inResponse: Response ) => {
     }
 });
 
+//Registro do path e do method para o endpoint que é utilizado para obter um post.
+//app.get() - Routes HTTP GET requests to the specified path with the specified callback functions
 app.get("/updatePost/:number", async (inRequest: Request ,inResponse: Response ) => {
     try {
         const post: IPost = await microposts.getPost(Number(inRequest.params.number));
@@ -55,6 +66,7 @@ app.get("/updatePost/:number", async (inRequest: Request ,inResponse: Response )
     }
 });
 
+//Registro do path e do method para o endpoint que é utilizado para dar update a um post em especifico.
 app.put("/updatePost/:number", async (inRequest: Request ,inResponse: Response ) => {
     try {
         const post : IPost = {
@@ -69,6 +81,7 @@ app.put("/updatePost/:number", async (inRequest: Request ,inResponse: Response )
     }
 });
 
+//Registro do path e do method para o endpoint que é utilizado para eliminar um post em especifico.
 app.delete("/:number", async (inRequest: Request ,inResponse: Response ) => {
     try {
         await microposts.deletePost(parseInt(inRequest.params.number));
--- server/src/posts.ts
+++ server/src/posts.ts
@@ -2,12 +2,16 @@ import * as path from "path";
 import Nedb from "nedb";
 const Datastore = require("nedb");
 
+//IPost é uma interface que descreve um post e que é necessária para adicionar, listar e eliminar operações de posts.
+//Quando se adiciona um post, caso não seja identificado um id, o NeDB irá associar um id automaticamente.
 export interface IPost {
     nmb: number;
     author: string,
     body: string,
 }
 
+// Um objeto do tipo NeDB Datastore, que será criado, bem como o path para a posts.db.
+// A Nedb carrega o ficheiro aotumaticamente caso ainda não exista é criado.
 export class Microposts {
     private db: Nedb;
     constructor() {
@@ -19,9 +23,10 @@ export class Microposts {
 
     public getPost(postNumber: number): Promise<IPost> {
         return new Promise((inResolve, inReject) => {
-            this.db.find({nmb: postNumber},
-                (inError: Error | null, inNewDoc: IPost) => {
-                    if (inError) {
+            this.db.find({nmb: postNumber}, // é chamado dentro da promise o método find() na DataStore no qual o resultado que será retornado
+                                            // é o de apenas um post em posts.db
+                (inError: Error | null, inNewDoc: IPost) => { // visto que sabe,os que os objetos que serao retornados sao do tipo IPost podemos usar como argumento o inNewDoc
+                    if (inError) {//rejeitamos a promise ou retornamos um objeto de documentos que contem os nossos objetos
                         inReject(inError);
                     } else {
                         inResolve(inNewDoc);
@@ -33,9 +38,10 @@ export class Microposts {
 
     public getPosts(): Promise<IPost[]> {
         return new Promise((inResolve, inReject) => {
-            this.db.find({},
-                (inError: Error | null, inNewDoc: IPost[]) => {
-                    if (inError) {
+            this.db.find({},// é chamado dentro da promise o método find() na DataStore no qual o resultado que será retornado
+                            // é o de todos e os dados em posts.db (que representa todos os posts)
+                (inError: Error | null, inNewDoc: IPost[]) => { // visto que sabe,os que os objetos que serao retornados sao do tipo IPost podemos usar como argumento o inNewDoc
+                    if (inError) {//rejeitamos a promise ou retornamos um objeto de documentos que contem os nossos objetos
                         inReject(inError);
                     } else {
                         inResolve(inNewDoc);
@@ -45,8 +51,8 @@ export class Microposts {
         });
     }
 
-    public addPost(inPost: IPost): Promise<IPost> {
-        return new Promise((inResolve, inReject) => {
+    public addPost(inPost: IPost): Promise<IPost> {     // inPost como primeiro argumento
+        return new Promise((inResolve, inReject) => {   // este método passa o objeto adicionaodo para a callback que irºa possuir um campo _id, sendo esse obejto rentornado caller e ao client, de maneira a aparecer no ecrã
             this.db.insert(inPost,
                 (inError: Error | null, inNewDoc: IPost) => {
                     if (inError) {
@@ -76,8 +82,9 @@ export class Microposts {
 
     public deletePost(nr: number): Promise<void> {
         return new Promise((inResolve, inReject) => {
-            this.db.remove({nmb: nr},{},
-                (inError: Error | null) => {
+            this.db.remove({nmb: nr},{}, // este método recebe o nr do post e é necessário que exista um post com esse nr
+                (inError: Error | null) => {// como a callback representa  o número de posts removidos, à partida será sempre 1
+                                            // entao desde que a promise nao seja rejeitada, o post será sempre removido com sucesso
                     if (inError) {
                         inReject(inError);
                     } else {
